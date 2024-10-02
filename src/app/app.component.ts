332e315a9698f5e66c909b4c12acadda3395ee41
ni.marchant <ni.marchant@duocuc.cl>
2024-10-02 20:51:13 -0300

    conexion bd - con errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SQLiteService } from './services/sqlite.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
-}
+  public isWeb: boolean = false;
+  private initPlugin!: boolean;
+  constructor(
+    private platform: Platform,
+    private sqlite: SQLiteService,
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(async () => {
+      this.sqlite.initializePlugin().then(async (ret) => {
+        this.initPlugin = ret;
+        if( this.sqlite.platform === "web") {
+          this.isWeb = true;
+          await customElements.whenDefined('jeep-sqlite');
+          const jeepSqliteEl = document.querySelector('jeep-sqlite');
+          if(jeepSqliteEl != null) {
+            await this.sqlite.initWebStore();
+            console.log(`>>>> isStoreOpen ${await jeepSqliteEl.isStoreOpen()}`);
+          } else {
+            console.log('>>>> jeepSqliteEl is null');
+          }
+        }
+
+        console.log(`>>>> in App  this.initPlugin ${this.initPlugin}`);
+      });
+    });
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SQLiteService } from './services/sqlite.service';
+import { AutenticationService } from './services/autentication.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    SQLiteService,
+    AutenticationService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
